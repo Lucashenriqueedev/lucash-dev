17e71dfdd8168195ec266955b4d9737f37b2aab7
LUCAS HENRIQUE DA SILVA SOUSA VIEIRA <lucas_hs_vieira@estudante.sesisenai.org.br>
2026-08-19 15:52:55 -0300

    teste

--- projetos/projeto_06/script.js
+++ projetos/projeto_06/script.js
@@ -1,7 +1,6 @@
 let tarefas = [];
 let contadorId = 0;
 
-
 let inputText = document.querySelector(".inputText");
 let btnEnviar = document.querySelector(".btnEnviar");
 let lista = document.querySelector(".lista");
@@ -23,6 +22,28 @@ function adicionarTarefa() {
         contadorId ++;
         tarefas.push({id: contadorId, tarefas: texto, concluido: false});
         inputText.value = ""
-        console.log(tarefas);
+        renderizar();
     }
+}
+
+function renderizar() {
+     lista.innerHTML = ""
+    tarefas.forEach(function(item){
+        let li = document.createElement("li");
+        li.textContent = item.tarefas;
+        lista.appendChild(li);
+
+        let btnExcluir = document.createElement("button");
+        btnExcluir.innerText = "❌"
+        li.appendChild(btnExcluir);
+        btnExcluir.addEventListener("click", function(e){
+            e.stopPropagation();
+
+            tarefas = tarefas.filter(function(t){
+                return t.id !== item.id
+
+            })
+            renderizar();
+        })
+    });
 }
